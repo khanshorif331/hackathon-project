fe1892056646c92756b1a0b3d79951e35bdf7279
khanshorif331 <khanshorif331@gmail.com>
2022-10-14 03:00:14 +0600

    folder structure completed

---
+++ src/Components/demoComp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const demoComp = () => {
+	return <div>just for keeping the folder structure</div>
+}
+
+export default demoComp
---
+++ src/hooks/demoHooks.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const demoHooks = () => {
+	return <div>just for keeping the folder structure</div>
+}
+
+export default demoHooks
