7424de0efcee91f511efb2fcc1af005df3bda9c0
khanshorif331 <khanshorif331@gmail.com>
2022-10-15 17:12:00 +0600

    simple navbar added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css'
 import Login from './Pages/Authentication/Login'
 import Register from './Pages/Authentication/Register'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
 
 function App() {
 	return (
 		<div>
 			<h1>Hello World</h1>
+			<Navbar></Navbar>
 			<Routes>
 				<Route path="/" element={<Login />} />
 				<Route path="/login" element={<Login />} />
--- src/Components/demoComp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const demoComp = () => {
-	return <div>just for keeping the folder structure</div>
-}
-
-export default demoComp
