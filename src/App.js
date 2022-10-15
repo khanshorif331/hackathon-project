3a55a2ee0d14b92ca409dca2b6ce1e675eee6e1c
khanshorif331 <khanshorif331@gmail.com>
2022-10-15 17:18:25 +0600

    simple dashboard setup done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './Pages/Authentication/Login'
 import Register from './Pages/Authentication/Register'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import DashboardPage from './Pages/Dashboard/DashboardPage/DashboardPage'
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Route path="/" element={<Login />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/dashboard" element={<DashboardPage />}></Route>
 			</Routes>
 		</div>
 	)
